53e70b3a27b29d2039a618435d3956b382b8dfe7
MatheusFariasSousa <143126173+MatheusFariasSousa@users.noreply.github.com>
2024-07-18 19:56:27 -0300

    nome arquivo sql

--- Exercicios-Beecrowd/PostgreSQL/2618.sql
+++
@@ -1,4 +0,0 @@
-select pro.name,vid.name,cat.name from products pro
-join providers vid on pro.id_providers=vid.id
-join categories cat on cat.id=pro.id_categories
-where vid.name='Sansul SA' and cat.name='Imported'
